c21d50ecf3b4b05e48605db5eff7d18883a5f072
alexcodri <alecscodreanu@gmail.com>
2020-05-23 12:29:56 +0300

    Initial Commit

---
+++ Weathery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weathery
+//
+//  Created by Alex Codreanu on 23/05/2020.
+//  Copyright © 2020 Alex Codreanu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
